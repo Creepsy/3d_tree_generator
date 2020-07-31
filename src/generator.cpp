cf1ecaa9498dd37e48ee4d8ad836fdad2525b9a5
Creepsy <Robin.gd@outlook.de>
2020-07-31 22:32:25 +0200

    added object creation support, test cube added

--- src/generator.cpp
+++ src/generator.cpp
@@ -3,8 +3,25 @@
 #include "object.h"
 
 int main() {
-    vertex v = {1.0, 2.34, 4};
-    face f = {1, 2, 1};
-    std::cout << v << f;
+    object cube = {"cube"};
+
+    cube.vertices.push_back(vertex{0, 0, 0});
+    cube.vertices.push_back(vertex{0, 1, 0});
+    cube.vertices.push_back(vertex{1, 0, 0});
+    cube.vertices.push_back(vertex{1, 1, 0});
+
+    cube.vertices.push_back(vertex{0, 0, 1});
+    cube.vertices.push_back(vertex{0, 1, 1});
+    cube.vertices.push_back(vertex{1, 0, 1});
+    cube.vertices.push_back(vertex{1, 1, 1});
+
+    cube.faces.push_back(face{1, 2, 4, 3});
+    cube.faces.push_back(face{5, 6, 8, 7});
+    cube.faces.push_back(face{1, 5, 6, 2});
+    cube.faces.push_back(face{3, 7, 8, 4});
+    cube.faces.push_back(face{2, 4, 8, 6});
+    cube.faces.push_back(face{1, 3, 7, 5});
+
+    std::cout << cube;
     return 0;
 }
